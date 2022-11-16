e88f97760a511d4d57f9e9c9e8c826c7ee1b8e14
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2022-11-15 23:20:37 -0600

    Update grupo-admins.js

--- plugins/grupo-admins.js
+++ plugins/grupo-admins.js
@@ -1,28 +1,19 @@
-let handler = async (m, { conn, participants, groupMetadata, args, usedPrefix, command }) => {
+let handler = async (m, { conn, participants, groupMetadata, args }) => {
 const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
-let oi = `*𝙈𝙀𝙉𝙎𝘼𝙅𝙀:* ${pesan}`
-let text = 
-`╭━━[ *𝙄𝙉𝙑𝙊𝘾𝘼𝙉𝘿𝙊 𝘼𝘿𝙈𝙄𝙉𝙎* ]━━━⬣ 
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
+let text = `*━「* 𝐈𝐍𝐕𝐎𝐂𝐀𝐍𝐃𝐎 𝐀𝐃𝐌𝐈𝐍𝐒 *」━*
 ${oi}
-
-*𝘼𝘿𝙈𝙄𝙉𝙎:*
+*𝙰𝙳𝙼𝙸𝙽𝚂:*
 ${listAdmin}
-
-𝙐𝙎𝘼𝙍 𝙀𝙉 𝘾𝘼𝙎𝙊 𝘿𝙀 𝙀𝙈𝙀𝙍𝙂𝙀𝙉𝘾𝙄𝘼
-╰━━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`.trim()
-
-await conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
-  
-await conn.sendHydrated(m.chat, null, `𝘼𝙙𝙢𝙞𝙣𝙨 | ${wm}`, null, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘', '.menu']
-], m)//, false, { mentions: [...groupAdmins.map(v => v.id), owner] })  
+*[ ⚠ ️] 𝚄𝚂𝙰𝚁 𝙴𝚂𝚃𝙴 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝚂𝙾𝙻𝙾 𝙲𝚄𝙰𝙽𝙳𝙾 𝚂𝙴 𝚃𝚁𝙰𝚃𝙴 𝙳𝙴 𝚄𝙽𝙰 𝙴𝙼𝙴𝚁𝙶𝙴𝙽𝙲𝙸𝙰!!*`.trim()
+conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
 handler.help = ['admins <texto>']
-handler.tags = ['group'] 
+handler.tags = ['group']
 handler.command = /^(admins|@admins|dmins)$/i
 handler.group = true
 export default handler
