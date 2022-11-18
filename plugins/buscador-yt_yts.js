234099116c4987599f08723df014edddc8b54900
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2022-11-18 16:02:12 -0600

    Toxi Testing

--- plugins/buscador-yt_yts.js
+++ plugins/buscador-yt_yts.js
@@ -1,7 +1,7 @@
 import yts from "yt-search"
 let handler = async (m, { text, conn, args, command, usedPrefix }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙀𝙇 𝙉𝙊𝙈𝘽𝙍𝙀 𝘿𝙀 𝙐𝙉 𝙑𝙄𝘿𝙀𝙊 𝙊 𝘾𝘼𝙉𝘼𝙇 𝘿𝙀 𝙔𝙊𝙐𝙏𝙐𝘽𝙀\n\n𝙒𝙍𝙄𝙏𝙀 𝙏𝙃𝙀 𝙉𝘼𝙈𝙀 𝙊𝙁 𝘼 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝙑𝙄𝘿𝙀𝙊 𝙊𝙍 𝘾𝙃𝘼𝙉𝙉𝙀𝙇`, fkontak,  m)
+if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝑬𝒔𝒄𝒓𝒊𝒃𝒂 𝒆𝒍 𝒏𝒐𝒎𝒃𝒓𝒆 𝒅𝒆𝒍 𝒗𝒊𝒅𝒆𝒐 𝒐 𝒄𝒂𝒏𝒂𝒍 𝒅𝒆 𝒚𝒐𝒖𝒕𝒖𝒃𝒆`, fkontak,  m)
 try {
 let search = await yts(args.join(" "))
 let listAudio = []
@@ -10,16 +10,16 @@ let listAudioDoc = []
 let listVideoDoc = []
 let teskd = `𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖 𝙙𝙚 *${args.join(" ")}*`
 
-const sections = [{ title: comienzo + ' 𝗔 𝗨 𝗗 𝗜 𝗢 ' + fin, rows: listAudio },
-{ title: comienzo + ' 𝗩 𝗜 𝗗 𝗘 𝗢 ' + fin, rows: listVideo },
-{ title: comienzo + ' 𝗔 𝗨 𝗗 𝗜 𝗢   𝗗 𝗢 𝗖 ' + fin, rows: listAudioDoc },
-{ title: comienzo + ' 𝗩 𝗜 𝗗 𝗘 𝗢   𝗗 𝗢 𝗖 ' + fin, rows: listVideoDoc }]
+const sections = [{ title: comienzo + ' 𝑨𝒖𝒅𝒊𝒐 ' + fin, rows: listAudio },
+{ title: comienzo + ' 𝑽𝒊𝒅𝒆𝒐 ' + fin, rows: listVideo },
+{ title: comienzo + ' 𝑨𝒖𝒅𝒊𝒐 𝗗 𝗢 𝗖 ' + fin, rows: listAudioDoc },
+{ title: comienzo + ' 𝑽𝒊𝒅𝒆𝒐 𝗗 𝗢 𝗖 ' + fin, rows: listVideoDoc }]
 
 const listMessage = {
 text: teskd,
 footer: '𝙀𝙡𝙞𝙟𝙖 𝙨𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖 𝙥𝙖𝙧𝙖 𝙥𝙤𝙙𝙚𝙧 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧\n' + wm,
 title: `${htki} *𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎* ${htka}`,
-buttonText: "🔎 𝗕 𝗨 𝗦 𝗖 𝗔 𝗥",
+buttonText: "🔎 𝑩𝒖𝒔𝒄𝒂𝒓",
 sections}
 
 for (let i of search.all) {
@@ -29,7 +29,7 @@ listVideo.push({title: i.title, description: `${i.author.name} | ${i.timestamp}`
 listVideoDoc.push({title: i.title, description: `${i.author.name} | ${i.timestamp}`, rowId: `${usedPrefix}ytmp4doc ${i.url}`})}
 conn.sendMessage(m.chat, listMessage, { quoted: fkontak })
 } catch (e) {
-m.reply(`${lenguajeGB['smsAvisoFG']()}𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘿𝙀 𝙉𝙐𝙀𝙑𝙊\n𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉`)
+m.reply(`${lenguajeGB['smsAvisoFG']()}¡𝑰𝒏𝒕𝒆𝒏𝒕𝒆 𝒅𝒆 𝒏𝒖𝒆𝒗𝒐!`)
 }}
 handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
 handler.tags = ['tools']
